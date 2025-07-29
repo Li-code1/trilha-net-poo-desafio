fb997de95343a194b358b2eff29e135e20979c13
Liliane Lima <lilianedelimasantos@yahoo.com.br>
2025-07-29 11:56:15 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
-﻿using DesafioPOO.Models;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+class Program
+{
+    static void Main()
+    {
+        Nokia nokia = new Nokia("123456789", "Nokia X20", "IMEI123456789", 128);
+        Iphone iphone = new Iphone("987654321", "iPhone 14", "IMEI987654321", 256);
+
+        nokia.Ligar();
+        nokia.InstalarAplicativo("WhatsApp");
+
+        iphone.ReceberLigacao();
+        iphone.InstalarAplicativo("Instagram");
+    }
+}
